16c1342792dd03aa60ce3a9d5641867be71d30a0
Slawek Lysakowski <slawku1@gmail.com>
2022-12-14 22:36:23 +0100

    update

--- src/main/java/pl/edu/wszib/vocabtool/dao/VocabDao.java
+++ src/main/java/pl/edu/wszib/vocabtool/dao/VocabDao.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pl.edu.wszib.vocabtool.model.Vocab;
 
-import java.util.ArrayList;
 
 @Repository
 public interface VocabDao extends JpaRepository<Vocab, Long> {
